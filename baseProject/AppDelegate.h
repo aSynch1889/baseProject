5d594bd934ea87feb3331a99b26a76fe71d06dac
aihua <liuaihuajm@126.com>
2016-05-30 13:39:09 +0800

    Initial Commit

---
+++ baseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baseProject
+//
+//  Created by aaaa on 16/5/30.
+//  Copyright © 2016年 <#AiHua Technology#> Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baseProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  baseProject
+//
+//  Created by aaaa on 16/5/30.
+//  Copyright © 2016年 <#AiHua Technology#> Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
